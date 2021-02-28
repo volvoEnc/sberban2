dbf8a35de5e07f5ae9556d1fa4dd1cdb85e93032
Danil Razumkov <razumkov@e1.ru>
2021-02-28 10:22:23 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', 'UserController@login');
 Route::middleware('auth:api')->group(function () {
     Route::post('/sber_request', 'SberController@request');
